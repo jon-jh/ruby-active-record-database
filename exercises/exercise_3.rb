50386eaa49dcc630aca10f05828fb1824f871583
jon-jh <hiebert456@gmail.com>
2024-11-16 12:22:12 -0600

    Part 3 and 4

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,9 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+# Store the 3rd store as @store3
+# Use destroy to delete the store from the database
+
+@store3=Store.find(3)
+@store3.destroy
+puts Store.count
