e9bae2bcbeb9c9da876f990a0c0c7cc102e217f1
jon-jh <hiebert456@gmail.com>
2024-11-16 12:06:50 -0600

    Update the name of a store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,13 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+# Assign store 1 and store 2 to instance variables
+# Then update store 1
+# It's not required to do this to use the update method
+
+
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+@store1.update(name: 'New Burnaby')
+
+puts "Store 1 new name: #{@store1.name}"
